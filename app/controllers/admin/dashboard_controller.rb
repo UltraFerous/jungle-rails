b922459043b11e6519874d8030b5df3be2b456ce
UltraFerous <jandoga31@gmail.com>
2023-12-13 21:51:13 -0500

    Improved the admin panel

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @product_count = Product.all.size
+    puts @product_count
+    @categories_count = Category.all.size
+    puts @categories_count
   end
 end
